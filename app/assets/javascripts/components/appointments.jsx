0e6fadce2480a31d5c0f3dd1e014b38214591b17
Ruslan <ruslan@MacBook-Pro-Ruslan.local>
2017-08-11 04:04:24 +0300

    install gem webpacker and react

---
+++ app/assets/javascripts/components/appointments.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Appointments = React.createClass({
+    render: function() {
+        return (
+            <h1>CalReact</h1>
+        )
+    }
+});
